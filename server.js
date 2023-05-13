8fdb91baf90e000cb204da2bc20487337cf0889f
Sriharsha Polimetla <38424697+S-Polimetla@users.noreply.github.com>
2023-05-13 19:08:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -27,4 +27,4 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log(`service listening on port: ` + port);
+console.log(`service listening on port: ` + port); 
